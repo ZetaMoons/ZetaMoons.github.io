c176bcd3f36541f08d35200244e0d5087bea07a1
ZetaMoons <65050918+ZetaMoons@users.noreply.github.com>
2021-07-15 16:53:28 -0300

    Create xmlreading.py

---
+++ xmlreading.py
@@ -0,0 +1,44 @@
+tree = etree.parse("Sample-employee-XML-file.xml")
+
+root = tree.getroot()
+columns = ["firstname", "lastname", "title", "division", "building","room"]
+
+datatframe = pd.DataFrame(columns = columns)
+
+for node in root: 
+
+    firstname = node.find("firstname").text
+
+    lastname = node.find("lastname").text 
+
+    title = node.find("title").text 
+    
+    division = node.find("division").text 
+    
+    building = node.find("building").text
+    
+    room = node.find("room").text
+    
+    datatframe = datatframe.append(pd.Series([firstname, lastname, title, division, building, room], index = columns), ignore_index = True)
+    datatframe.to_csv("employee.csv", index=False)
+    
+    # importing PIL 
+from PIL import Image 
+
+import urllib.request
+# Downloading dataset
+urllib.request.urlretrieve("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg", "dog.jpg")
+# Read image 
+img = Image.open('dog.jpg') 
+  
+# Output Images 
+display(img)
+
+
+# Import pandas library
+import pandas as pd
+path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/diabetes.csv"
+df = pd.read_csv(path)
+# show the first 5 rows using dataframe.head() method
+print("The first 5 rows of the dataframe") 
+df.head(5)
